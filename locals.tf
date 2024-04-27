0dd4e36aef62ca4f5181767bf5796817c8eda6e0
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-04-27 13:49:16 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -9,6 +9,17 @@ locals{
   length_firstname=length(local.firstname)
   length_lastname=length(local.lastname)
   length_city=length(local.city)
+  #Homework from Friday April 26 2024
+  landmark1="Saint-Joseph Oratory"
+  landmark2="Notre-Dame Basilica"
+  landmark3="Bell Centre"
+  landmark4="Olympic Stadium"
+  landmark5="Port of Old Montreal"
+  length_landmark1=length(local.landmark1)
+  length_landmark2=length(local.landmark2)
+  length_landmark3=length(local.landmark3)
+  length_landmark4=length(local.landmark4)
+  length_landmark5=length(local.landmark5)
 }
 output "print"{
     value = local.servicename
@@ -39,4 +50,35 @@ output "lengthlastname"{
 }
 output "lengthcity"{
     value = local.length_city
+}
+#Homework from Friday April 26 2024
+output "landmark1"{
+    value = local.landmark1
+}
+output "landmark2"{
+    value = local.landmark2
+}
+output "landmark3"{
+    value = local.landmark3
+}
+output "landmark4"{
+    value = local.landmark4
+}
+output "landmark5"{
+    value = local.landmark5
+}
+output "length_landmark1"{
+    value = local.length_landmark1
+}
+output "length_landmark2"{
+    value = local.length_landmark2
+}
+output "length_landmark3"{
+    value = local.length_landmark3
+}
+output "length_landmark4"{
+    value = local.length_landmark4
+}
+output "length_landmark5"{
+    value = local.length_landmark5
 }
