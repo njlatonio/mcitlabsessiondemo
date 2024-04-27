8c8b74833c0c019530186b42c8118bf490f3272a
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-04-27 13:27:23 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,10 +1,22 @@
 locals{
   servicename="abracadabra"
   forum="abracadabramcit"
+  firstname="nichelle"
+  lastname="latonio"
+  city="montreal"
 }
 output "print"{
     value = local.servicename
 }
 output "secondprint"{
     value = local.forum
+}
+output "firstnameprint"{
+    value = local.firstname
+}
+output "lastnameprint"{
+    value = local.lastname
+}
+output "cityprint"{
+    value = local.city
 }
