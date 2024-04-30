460559326e040d42c4130e65faae87b504410e93
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-04-29 20:13:12 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,69 @@
+output "print"{
+    value = local.servicename
+}
+output "secondprint"{
+    value = local.forum
+}
+output "firstnameprint"{
+    value = local.firstname
+}
+output "lastnameprint"{
+    value = local.lastname
+}
+output "cityprint"{
+    value = local.city
+}
+output "lengthsa"{
+    value = local.lengthsa
+}
+output "lengthforum"{
+    value = local.lengthforum
+}
+output "lengthfirstname"{
+    value = local.length_firstname
+}
+output "lengthlastname"{
+    value = local.length_lastname
+}
+output "lengthcity"{
+    value = local.length_city
+}
+#Homework from Friday April 26 2024
+output "landmark1"{
+    value = local.landmark1
+}
+output "landmark2"{
+    value = local.landmark2
+}
+output "landmark3"{
+    value = local.landmark3
+}
+output "landmark4"{
+    value = local.landmark4
+}
+output "landmark5"{
+    value = local.landmark5
+}
+output "length_landmark1"{
+    value = local.length_landmark1
+}
+output "length_landmark2"{
+    value = local.length_landmark2
+}
+output "length_landmark3"{
+    value = local.length_landmark3
+}
+output "length_landmark4"{
+    value = local.length_landmark4
+}
+output "length_landmark5"{
+    value = local.length_landmark5
+}
+*/
+
+#Class Monday April 29 2024
+output "wintersportslist"{
+  value = [for sport in local.winterlistOfSports:sport]
+}
+
 
