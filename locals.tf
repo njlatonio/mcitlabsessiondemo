beb0aee27864266f8a2a4bb11f6e299405ab06c2
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-04-29 20:12:59 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals{
-  /*
+  #Class Friday April 26 2024
   servicename="abracadabra"
   forum="abracadabramcit"
   lengthsa=length(local.servicename)
@@ -22,77 +22,8 @@ locals{
   length_landmark3=length(local.landmark3)
   length_landmark4=length(local.landmark4)
   length_landmark5=length(local.landmark5)
-  */
+  
 
   #Class Monday April 29 2024
   winterlistOfSports=["icehockey", "snowboarding", "iceskating"]
 }
-/*
-output "print"{
-    value = local.servicename
-}
-output "secondprint"{
-    value = local.forum
-}
-output "firstnameprint"{
-    value = local.firstname
-}
-output "lastnameprint"{
-    value = local.lastname
-}
-output "cityprint"{
-    value = local.city
-}
-output "lengthsa"{
-    value = local.lengthsa
-}
-output "lengthforum"{
-    value = local.lengthforum
-}
-output "lengthfirstname"{
-    value = local.length_firstname
-}
-output "lengthlastname"{
-    value = local.length_lastname
-}
-output "lengthcity"{
-    value = local.length_city
-}
-#Homework from Friday April 26 2024
-output "landmark1"{
-    value = local.landmark1
-}
-output "landmark2"{
-    value = local.landmark2
-}
-output "landmark3"{
-    value = local.landmark3
-}
-output "landmark4"{
-    value = local.landmark4
-}
-output "landmark5"{
-    value = local.landmark5
-}
-output "length_landmark1"{
-    value = local.length_landmark1
-}
-output "length_landmark2"{
-    value = local.length_landmark2
-}
-output "length_landmark3"{
-    value = local.length_landmark3
-}
-output "length_landmark4"{
-    value = local.length_landmark4
-}
-output "length_landmark5"{
-    value = local.length_landmark5
-}
-*/
-
-#Class Monday April 29 2024
-output "wintersportslist"{
-  value = [for sport in local.winterlistOfSports:sport]
-}
-
