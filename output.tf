a1314da35750a7e28d126078869262ec8bf0ca01
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-01 20:09:10 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -156,9 +156,9 @@ output "reverse_number" {
 }
 
 output "string_contains_no_sample" {
-  value = local.contains_keyword ? "String contains 'Mcit'" : "String does not contain 'Mcit'"
+  value = local.contains_keyword "Mcit" "String contains 'Mcit'" : "String does not contain 'Mcit'"
 }
 
 output "string_contains_sample" {
-  value = local.contains_keyword ? "String contains 'Nichelle'" : "String does not contain 'Nichelle'"
+  value = local.contains_keyword "Nichelle" "String contains 'Nichelle'" : "String does not contain 'Nichelle'"
 }
