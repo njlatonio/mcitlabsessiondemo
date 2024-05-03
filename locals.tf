2c531aa9351da844541a6ae57a485f34ec7e4811
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-03 19:38:42 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -39,5 +39,11 @@ locals{
   //Class Friday May 3 2024-------------------------------------------------------------------------------------------------------------
   simple_local_flattened_list = flatten(var.simple_nested_list)
   flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])
+  characters = ["luke", "yoda", "darth"]
+  enemies_destroyed = [4252, 900, 20000056894]
+  character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
+      character => local.enemies_destroyed[index]
+  }
+
 }
 
