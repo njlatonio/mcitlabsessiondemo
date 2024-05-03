b0424b10076c093b0c812294e45332fe577e7eab
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-02 23:31:19 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -166,5 +166,5 @@ output "string_contains_sample" {
 
 //Homework Wednesday May 1 2024-------------------------------------------------------------------------------------------------------
 output "five_countries_list" {
-    value = join("+", var.five_countries)
+    value = var.five_countries
 }
