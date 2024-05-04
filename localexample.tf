7a78d916fa8c22100cd289e2530f6bfa07e4e7cb
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-03 20:23:45 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -1,1 +1,38 @@
+/*
+//Class Friday May 3 2024------------------------------------------------------------------------------------------------------------------
+variable "simple_nested_list" {
+  type    = list(list(string))
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]
+}
+variable "nested_list" {
+  type    = list(list(string))
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]
+}
 
+variable "nested_map" {
+  type    = map(map(string))
+  default = {
+    group1 = {"name" = "Alice", "age" = "25"}
+    group2 = {"name" = "Bob", "age" = "30"}
+  }
+}
+
+locals{
+  servicename="abracadabra"
+  forum="abracadabramcit"
+  lengthsa=length(local.servicename)
+  lengthforum=length(local.forum)
+  winterlistOfSports=["icehockey","snowboarding","iceskating"]
+  total_output = ["150", "150", "150"]
+  characters = ["luke", "yoda", "darth"]
+  enemies_destroyed = [4252, 900, 20000056894]
+  character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
+      character => local.enemies_destroyed[index]
+  }
+  simple_local_flattened_list = flatten(var.simple_nested_list)
+  flattened_list = flatten(var.nested_list)
+  flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])
+  sum_element1=20
+  sum_element2=30
+}
+*/
