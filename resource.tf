c73a9ccdaccd80a6687c6bd759ce41cb9ccbc464
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-06 20:51:17 -0400

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -3,7 +3,30 @@ resource "azurerm_resource_group" "nichelle_azureresourcegroup1" {
   name     = var.name1
   location = var.location1
 }
+
 resource "azurerm_resource_group" "nichelle_azureresourcegroup2" {
   name     = local.loc_name
   location = local.loc_location
+}
+
+resource "azurerm_kubernetes_cluster" "batchabcd" {
+  for_each            = {for cluster in local.cluster_names: cluster=>cluster}
+  name                = "${var.prefix}cluster"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
 }
