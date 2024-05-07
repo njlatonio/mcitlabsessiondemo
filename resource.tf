7fc7f901ece6eecbfa6dd2ce99f3c033475c31f2
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-06 20:46:28 -0400

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,9 @@
+
+resource "azurerm_resource_group" "nichelle_azureresourcegroup1" {
+  name     = var.name1
+  location = var.location1
+}
+resource "azurerm_resource_group" "nichelle_azureresourcegroup2" {
+  name     = local.loc_name
+  location = local.loc_location
+}
