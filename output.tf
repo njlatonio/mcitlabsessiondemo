d155f4f749d9a67888a8d68d6d245298b0ce5de4
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-06 20:54:37 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -306,3 +306,44 @@ output "sum_flatten_number_list" {        #Question 13
 }
 
 */
+//Class Monday May 6 2024----------------------------------------------------------------------------------------------------------
+output "id" {
+  value = [
+    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.id
+  ]
+}
+ 
+output "kube_config" {
+  sensitive = true
+  value = [ 
+    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config_raw
+  ]
+}
+ 
+output "client_key" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config.0.client_key
+  ]
+}
+ 
+output "client_certificate" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config.0.client_certificate
+  ]
+}
+ 
+output "cluster_ca_certificate" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config.0.cluster_ca_certificate
+  ]
+}
+ 
+output "host" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config.0.host
+  ]
+}
