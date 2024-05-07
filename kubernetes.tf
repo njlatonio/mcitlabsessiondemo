823bfa5da61a72bdcb6175e4c86bce3328330514
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-06 20:21:05 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -3,6 +3,6 @@ locals{
   cluster_names=["k8batcha06","k9batcha06","k10batcha06","k11batcha06","k12batcha06"]
 }
 resource "azurerm_resource_group" "nichelle_azureresourcegroup" {
-  name     = "MCIT_resource_group"
-  location = "Canada Central"
+  name     = var.name
+  location = var.location
 }
