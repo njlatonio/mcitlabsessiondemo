15a4e9a196ec58f5233f9e6c3967cdee002e1d95
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-08 19:33:59 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -186,6 +186,5 @@ variable "location1" {
 
 //Class Wednesday May 8 2024-------------------------------------------------------------------------------------------------------------
 variable "clusterlist" {
-  type = string
   default = ["montrealcluster", "torontocluster", "vancouvercluster", "albertacluster"]
 }
