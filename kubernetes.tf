f5bfdba37709ce1c1bab1668eeb2786139021425
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-08 20:31:15 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -46,4 +46,36 @@ output "kube_id"{
 output "kube_name"{
   value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.name ]
 }
+
+locals{ 
+  cluster_names=["k8batcha06","k9batcha06","k10batcha06","k11batcha06","k12batcha06"]
+}
+resource "azurerm_resource_group" "azureresourcegroup" {
+  name     = "MCIT_resource_group"
+  location = "Canada Central"
+}
+
+
+//Class Wednesday May 8 2024-------------------------------------------------------------------------------------------------------------
+resource "azurerm_kubernetes_cluster" "batchabcd" {
+  for_each            = {for cluster in var.classworkclusters: cluster=>cluster}
+  name                = "${var.prefix}${each.key}"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = var.dnsprefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
 */
