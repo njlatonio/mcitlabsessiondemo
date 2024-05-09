453cc515a112a25c9a8cfaf05a2cc266adbb9e6b
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-08 20:36:13 -0400

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -149,3 +149,37 @@ resource "azurerm_kubernetes_cluster" "citiescluster" {
  
 }
 
+resource "azurerm_kubernetes_cluster" "canadacluster" {
+  for_each            = {for cluster in var.clustercitylist: cluster=>cluster}
+  name                = "${var.prefix1}cluster"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = var.default_node_pool_name
+    node_count = var.default_node_pool_node_count
+    vm_size    = var.default_node_pool_vm_size
+  }
+
+  identity {
+    type = var.identity
+  }
+
+ tags = {
+    Environment = var.environment_tag
+  }
+ 
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
+ for_each               = azurerm_kubernetes_cluster.canadacluster
+ name                   = "${each.key}"
+ kubernetes_cluster_id  = each.value.id
+ vm_size                = var.default_node_pool_vm_size
+ node_count             = var.default_node_pool_node_count
+
+  tags = {
+    Environment = var.environment_tag
+  }
+}
