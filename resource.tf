130027c89308d479f91aa0cf245ca69e88e3763b
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-13 18:58:36 -0400

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -190,20 +190,20 @@ resource "azurerm_kubernetes_cluster" "batchabcd" {
   name                = "${var.prefix}cluster"
   location            = azurerm_resource_group.azureresourcegroup.location
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  dns_prefix          = "exampleaks1"
+  dns_prefix          = var.dns_prefix
 
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+    name       = var.default_node_pool_name
+    node_count = var.default_node_pool_node_count
+    vm_size    = var.default_node_pool_vm_size
   }
 
   identity {
-    type = "SystemAssigned"
+    type = var.identity
   }
 
  tags = {
-    Environment = "Production"
+    Environment = var.environment_tag
   }
  
 }
